d98524831e37e81b03f46becdc21966fd714cfc7
ecv-elishanecesito <144196323+ElishaNecesito@users.noreply.github.com>
2024-12-13 17:03:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World Elisha!</p>"
 
 def allowed_file(filename):
     return '.' in filename and \
